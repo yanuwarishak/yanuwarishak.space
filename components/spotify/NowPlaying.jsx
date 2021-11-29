7c14775d9a47115dc489d0c57d6e36656a98ea43
Yanuwar Ishak <yanuwarishak@gmail.com>
2021-11-29 13:01:54 +0700

    updating mobile now playing

--- components/spotify/NowPlaying.jsx
+++ components/spotify/NowPlaying.jsx
@@ -27,28 +27,26 @@ export default function NowPlaying() {
   return (
     <div className="p-4 w-full bg-[#1DB954] text-black">
       {song.isPlaying ? (
-        <div className="flex flex-row items-center">
-          <div className="w-5 xs:w-10 mr-4">
+        <div className="flex flex-col relative">
+          <div className="absolute -top-1 -right-1 w-5">
             <SpotifyIcon />
           </div>
-          <div className="flex flex-col">
-            <h1 className="text-sm text-normal">Now Playing:</h1>
-            {song.link && (
-              <a href={song.link} target="_blank" rel="noopener noreferrer">
-                <h2 className="font-semibold text-normal md:text-lg hover:underline cursor-pointer">
-                  {song.title}
-                </h2>
-              </a>
-            )}
-            {!song.link && (
+          <h1 className="text-sm text-normal">Now Playing:</h1>
+          {song.link && (
+            <a href={song.link} target="_blank" rel="noopener noreferrer">
               <h2 className="font-semibold text-normal md:text-lg hover:underline cursor-pointer">
                 {song.title}
               </h2>
-            )}
-            <h2 className="text-sm align-text-bottom">
-              by {song.artist ? song.artist : "Local File"}
+            </a>
+          )}
+          {!song.link && (
+            <h2 className="font-semibold text-normal md:text-lg hover:underline cursor-pointer">
+              {song.title}
             </h2>
-          </div>
+          )}
+          <h2 className="text-sm align-text-bottom">
+            by {song.artist ? song.artist : "Local File"}
+          </h2>
         </div>
       ) : (
         <div className="flex flex-col gap-2">
