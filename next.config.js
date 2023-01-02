99c2556676741fe58e4feef15cc632dffeb077ae
Yanuwar Ishak <yanuwarishak@gmail.com>
2023-01-02 11:39:08 +0700

    remove preact from prod build

--- next.config.js
+++ next.config.js
@@ -4,16 +4,4 @@ module.exports = {
     domains: ["i.scdn.co"],
     deviceSizes: [480, 640],
   },
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
 };
